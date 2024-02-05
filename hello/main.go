5b45b3474e7a6d4a927c7f252a7414836c899743
Nadav Ben Mazia <nadavbm@outlook.com>
2024-02-05 13:31:16 +0100

    improve topics

--- hello/main.go
+++ hello/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 // SayHello will send hello to channel and print the value received from the channel
 func SayHello() {
+	fmt.Println("\nstart hello")
 	ch := make(chan string)
 	msg := "hello"
 	go func() {
--- topics/topics.go
+++ topics/topics.go
@@ -6,19 +6,41 @@ import (
 )
 
 // TimerSize in seconds
-const TimerSize = 4
+const TimerSize = 6
 
 // SendReceiveTopics will send and receive from a topic
 func SendReceiveTopics() {
+	fmt.Println("\nstart topics")
 	topics := make(map[string](chan string))
 
-	topics["farm"] = make(chan string, 2)
+	topics["farm"] = make(chan string, 3)
 	msg := "hello farmer"
 
+	var stop = false
 	go func() {
 		for {
-			fmt.Println("send to channel")
+			now := time.Now()
+			time.Sleep(1 * time.Second)
+			if stop {
+				fmt.Println("timer stopped", now.Format(time.UnixDate))
+				break
+			}
+			fmt.Println("send to channel", msg, now.Format(time.UnixDate))
 			topics["farm"] <- msg
+		}
+	}()
+
+	msg2 := "yeah"
+	go func() {
+		for {
+			now := time.Now()
+			time.Sleep(1 * time.Second)
+			if stop {
+				fmt.Println("timer stopped", now.Format(time.UnixDate))
+				break
+			}
+			fmt.Println("send to channel", msg2, now.Format(time.UnixDate))
+			topics["farm"] <- msg2
 		}
 	}()
 
@@ -28,10 +50,11 @@ func SendReceiveTopics() {
 	for {
 		select {
 		case <-topics["farm"]:
-			fmt.Println(<-topics["farm"])
+			fmt.Println("message offload from farm channel", <-topics["farm"], time.Now().Format(time.UnixDate))
 		case <-timer.C:
+			stop = true
 			timer.Stop()
-			fmt.Println(fmt.Sprintf("Pubsub is closed after %v", duration))
+			fmt.Println(fmt.Sprintf("Pubsub is closed after %v, channel length %d", duration, len(topics)), time.Now().Format(time.UnixDate))
 			time.Sleep(1 * time.Second)
 			close(topics["farm"])
 			return
